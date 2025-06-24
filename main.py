c26e19f80d6dd8e60bbb3e8faff44a1255a7adb9
lswlover2579 <354058870@qq.com>
2025-06-24 12:37:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2443,16 +2443,10 @@ class NewsAnalyzer:
 
         ids = [
             ("toutiao", "今日头条"),
-            ("baidu", "百度热搜"),
             ("wallstreetcn-hot", "华尔街见闻"),
             ("thepaper", "澎湃新闻"),
-            ("bilibili-hot-search", "bilibili 热搜"),
             ("cls-hot", "财联社热门"),
-            ("ifeng", "凤凰网"),
-            "tieba",
-            "weibo",
-            "douyin",
-            "zhihu",
+            ("ifeng", "凤凰网")
         ]
 
         print(f"开始爬取数据，请求间隔 {self.request_interval} 毫秒")
